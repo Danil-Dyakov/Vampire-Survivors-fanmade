78a5368202451a08542cc9f672d35f525a6c2274
Chernov Alexandr <chernovalexandr2010@bk.ru>
2025-01-15 20:12:03 +0300

    Добавил луд имедж, и в бейсике делаю окно.

--- basic_main.py
+++ basic_main.py
@@ -20,6 +20,8 @@ def main() -> None:
     screen = pygame.display.set_mode(size)
 
     start_screen(screen)
+    play(screen)
+    terminate()
 
 
 #    while True:
@@ -30,7 +32,6 @@ def main() -> None:
 
 
 def start_screen(screen: Surface) -> None:
-
     clock = Clock()
     background = pygame.transform.scale(load_image('start_screen.png'), screen.get_size())
     screen.blit(background, (0, 0))
@@ -47,6 +48,7 @@ def start_screen(screen: Surface) -> None:
 
 
 def play(screen: Surface) -> None:
+    player, level_x, level_y = generate_level(load_level('map.txt'))
     clock = pygame.time.Clock()
 
     running = True
@@ -60,6 +62,65 @@ def play(screen: Surface) -> None:
         pygame.display.flip()
         clock.tick(100)
     pygame.quit()
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    max_width = max(map(len, level_map))
+
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+player_image = load_image('mario.png')
+
+tile_width = tile_height = 50
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+
+# основной персонаж
+player = None
+
+# группы спрайтов
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
 
 
 def terminate():
